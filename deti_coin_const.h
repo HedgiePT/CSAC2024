083c66279d599ebeffbe07b13709974b7c9fc76d
João Diogo Craveiro Ferreira <devilj@outlook.pt>
2024-12-03 20:54:19 +0000

    AVX: Usar métodos do professor.

--- deti_coin_const.h
+++ deti_coin_const.h
@@ -4,13 +4,14 @@
 
 // First bytes of every coin.
 // Spells "DETI coin   ", but flipped (due to endianess).
-const uint32_t COIN_STD_HEADER[3] = {
+const uint32_t k_coin_std_header[3] = {
     0x49544544, // ITED
     0x696f6320, // ioc␣
     0x2020206e  // ␣␣␣n
 };
 
 // const unsigned int k_deti_coin_header_length = 10;
-const int COIN_LENGTH = 52;
-const int COIN_BODY_START = 10;
-const int COIN_BODY_END = COIN_LENGTH - 1;
+const int k_coin_length = 52;
+const int k_coin_body_start = 10;
+const int k_coin_body_end = k_coin_length - 1;
+const int k_coin_body_length = k_coin_body_end - k_coin_body_start;
--- deti_coins_cpu_avx_search.h
+++ deti_coins_cpu_avx_search.h
@@ -18,25 +18,36 @@
 // Includes importantes
 #include <inttypes.h>
 #include "deti_coin_const.h"
+#include <stdio.h>
 
 // Vetor de 4 signed ints de 32 bits (4 bytes) cada.
 typedef int32_t i32x4 __attribute__ ((vector_size (16)));   // 4*4 = 16
 
 #define BROADCAST(x) ((i32x4){x, x, x, x})
-#define LENGTH(x) (sizeof(x) / sizeof(x[0]))
+#define BROADCAST_INC(x) ((i32x4){x, x+1, x+2, x+3})
 
-
-static void init_coins(i32x4 *v4array) {
-    v4array[0] = BROADCAST((int32_t)COIN_STD_HEADER[0]);
-    v4array[1] = BROADCAST((int32_t)COIN_STD_HEADER[1]);
-    v4array[2] = BROADCAST((int32_t)COIN_STD_HEADER[2]);
-
-    for (int i = 3; i < 12; i++) {
-        v4array[i] = BROADCAST(' ');
-    }
-
-    v4array[12] = BROADCAST('\n');
-}
+#define NEXT_VALUE_TO_TRY(v)                                                   \
+do                                                                             \
+{                                                                              \
+    v++;                                                                       \
+                                                                               \
+    if ((v & 0xFF) == 0x7F)                                                    \
+    {                                                                          \
+        v += 0xA1;                                                             \
+        if (((v >> 8) & 0xFF) == 0x7F)                                         \
+        {                                                                      \
+            v += 0xA1 << 8;                                                    \
+            if  (((v >> 16) & 0xFF) == 0x7F)                                   \
+            {                                                                  \
+                v += 0xA1 << 16;                                               \
+                if (((v >> 24) & 0xFF) == 0x7F)                                \
+                {                                                              \
+                    v += 0xA1 << 24;                                           \
+                }                                                              \
+            }                                                                  \
+        }                                                                      \
+    }                                                                          \
+} while (0)
 
 
 static void deti_coins_cpu_avx_search(uint32_t n_random_words)
@@ -44,25 +55,55 @@ static void deti_coins_cpu_avx_search(uint32_t n_random_words)
     i32x4 v4hash[4];    // Hash tem 128 bits de comprimento.
     i32x4 v4coin[13];   // 13 == 52 bytes / sizeof(int32_t) == 52/4
 
-    init_coins(v4coin);
+    // INIT v4coin
+    v4coin[0] = BROADCAST((int32_t)k_coin_std_header[0]);
+    v4coin[1] = BROADCAST((int32_t)k_coin_std_header[1]);
+    v4coin[2] = BROADCAST((int32_t)k_coin_std_header[2]);
+    for (int i = 3; i < 10; i++) {
+        v4coin[i] = BROADCAST(0x20202020);
+    }
+    int32_t v1 = 0x7e7e7e70;
+    int32_t v2 = 0x20202020;
+    v4coin[10] = BROADCAST(v1);
+    v4coin[11] = BROADCAST(v2);
+    v4coin[12] = BROADCAST_INC(0x0a202020);
+
+    /* IDEA PARA GERAR MODEAS:
+     *
+     * Cada lane começa com um prefixo diferente;
+     * Depois, para cada iteração, geramos uma string com os X caracteres
+     * restantes (corpo - prefixo), e copíamos essa string para todas as
+     * lanes.
+     */
 
     uint64_t n_attempts = 0, n_coins = 0;
 
     while (!stop_request) {
+        //printf("v4coin[10]=%x, [11]=%x, [12]=%x\n", v4coin[10][3], v4coin[11][3], v4coin[12][3]);
         md5_cpu_avx(v4coin, v4hash);
+        for(uint32_t lane = 0; lane < 4; lane++)
+            if(v4hash[3][lane] == 0)
+            {
+                uint32_t coin[13];
+                for(uint32_t idx = 0; idx < 13; idx++)
+                    coin[idx] = v4coin[idx][lane];
+                save_deti_coin(coin);
+                n_coins++;
+            }
 
-        // Implementar resto do código aqui...
+        NEXT_VALUE_TO_TRY(v1);
+        v4coin[10] = BROADCAST(v1);
+        if (v1 == 0x20202020)
+        {
+            NEXT_VALUE_TO_TRY(v2);
+            v4coin[11] = BROADCAST(v2);
+        }
 
-        /* IDEA PARA GERAR MODEAS:
-         *
-         * Cada lane começa com um prefixo diferente;
-         * Depois, para cada iteração, geramos uma string com os X caracteres
-         * restantes (corpo - prefixo), e copíamos essa string para todas as
-         * lanes.
-         */
-
-        n_attempts++;
+        n_attempts += 4;
     }
+
+    STORE_DETI_COINS();
+    printf("deti_coins_cpu_avx_search: %lu DETI coin%s found in %lu attempt%s (expected %.2f coins)\n",n_coins,(n_coins == 1ul) ? "" : "s",n_attempts,(n_attempts == 1ul) ? "" : "s",(double)n_attempts / (double)(1ul << 32));
 
 }
 
